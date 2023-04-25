3a86fb10710a146f25fb748ceedb20caecc1fb5a
Siri <128618015+shwetha765@users.noreply.github.com>
2023-04-26 02:10:58 +0530

    Create Employee and Department Model

--- employee_api/models.py
+++ employee_api/models.py
@@ -13,3 +13,4 @@ class Employee(models.Model):
     employee_name = models.CharField(max_length=255)
     designation = models.CharField(max_length=255)
     department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+ 
